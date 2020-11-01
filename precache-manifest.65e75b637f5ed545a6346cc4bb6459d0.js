826be8c8d7cf831dc9279f939951c86d4c8146d2
Marian Dandarila <marian_dandarila@yahoo.com>
2020-11-01 17:16:00 +0200

    Updates

---
+++ precache-manifest.65e75b637f5ed545a6346cc4bb6459d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1297d743db5aa4999e79cdbe1fc926d2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4055d3051e3df5ab069",
+    "url": "/monsters-rolodex/static/css/main.c9d4bf62.chunk.css"
+  },
+  {
+    "revision": "0ebb722af33ec599140e",
+    "url": "/monsters-rolodex/static/js/2.c27b6d93.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/monsters-rolodex/static/js/2.c27b6d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4055d3051e3df5ab069",
+    "url": "/monsters-rolodex/static/js/main.b2d676cc.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
